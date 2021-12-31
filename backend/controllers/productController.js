b6fe8c94735dbc722b5b77f3ef19299ed413071a
sank48 <sanksrn48@gmail.com>
2021-12-31 20:06:26 +0530

    Display a single product by id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,7 +10,7 @@ exports.newProduct = async (req, res, next)=>{
 	})
 }
 
-// Get all Products
+// Get all Products => api/v1/products
 exports.getProducts = async (req,res,next)=>{
 	const product = await Product.find();
 	res.status(200).json({
@@ -19,4 +19,21 @@ exports.getProducts = async (req,res,next)=>{
 		product
 		//message: 'This route will show all products in database.'
 	})
+}
+
+// Get single product detail => api/v1/products/:id
+exports.getSingleProduct = async (req, res, next)=>{
+	const product = await Product.findById(req.params.id);
+
+	if(!product){
+		res.status(404).json({
+			success: false,
+			message: "No product found with given ID."
+		})
+	}else{
+		res.status(200).json({
+			success: true,
+			product
+		})
+	}
 }
