1d7dac1567acb5bd1108f21887edb5b390b22137
sank48 <sanksrn48@gmail.com>
2022-01-08 20:12:44 +0530

    Separated errors in production mode and development mode

--- backend/middleware/errors.js
+++ backend/middleware/errors.js
@@ -4,10 +4,21 @@ const errorHandler = require('../utils/errorHandler');
 
 module.exports = (err ,req, res, next)=>{
 	err.statusCode = err.statusCode || 500;
-	err.message = err.message || "Internal Server Error"
 
-	res.json({
-		success: false,
-		error: err.stack
-	})
+	if(process.env.NODE_ENV==="DEVELOPMENT"){
+		res.status(err.statusCode).json({
+			success: false,
+			error: err,
+			errorMessage: err.message,
+			stack: err.stack
+		})
+	}
+
+	if(process.env.NODE_ENV==="PRODUCTION"){
+		res.json({
+			success: false,
+			message: err.message || "Internal Server Error"
+		})
+	}
+	
 }
