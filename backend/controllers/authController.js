dd0535e5dddbf741dd0518ba2dfbf7604c80b350
sank48 <sanksrn48@gmail.com>
2022-01-21 00:26:54 +0530

    Storing token to cookies

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncError = require('../middleware/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken')
 
 exports.registerUser = catchAsyncError(async(req,res,next)=>{
 	const {name, email, password} = req.body;
@@ -15,12 +16,7 @@ exports.registerUser = catchAsyncError(async(req,res,next)=>{
 		}
 	})
 
-	const token = user.getJwtToken();
-
-	res.status(201).json({
-		success: true,
-		token
-	})
+	sendToken(user, 200, res);
 })
 
 // Login user => /api/v1/login
@@ -46,12 +42,7 @@ exports.loginUser = catchAsyncError(async(req,res,next)=>{
 		return next(new ErrorHandler('Invalid Email or Password', 401));
 	}
 
-	const token = user.getJwtToken();
-
-	res.status(200).json({
-		success: true,
-		token
-	})
+	sendToken(user, 200, res);
 
 
 })
