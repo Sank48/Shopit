ae1ac6ff8026708ede70c2e53517a6cfc6475cb1
sank48 <sanksrn48@gmail.com>
2022-01-23 16:59:00 +0530

    Authorize user roles and permission

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,15 +4,15 @@ const router = express.Router();
 
 const{ getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
 
-const { isAuthenticatedUser }=require('../middleware/auth');
+const { isAuthenticatedUser, authorizeRoles }=require('../middleware/auth');
 
 router.route('/products').get(getProducts);
 router.route('/products/:id').get(getSingleProduct);
 
-router.route('/admin/products/new').post(isAuthenticatedUser, newProduct);
+router.route('/admin/products/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
 router.route('/admin/products/:id')
-			.put(isAuthenticatedUser, updateProduct)
-			.delete(isAuthenticatedUser, deleteProduct);
+			.put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
+			.delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 
 
 module.exports = router;
