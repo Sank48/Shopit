bfda993ec3c3073360618ccc509d1d65afcd6ad4
sank48 <sanksrn48@gmail.com>
2022-01-23 16:27:06 +0530

    Added authorization to admins

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,13 +6,13 @@ const{ getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct }
 
 const { isAuthenticatedUser }=require('../middleware/auth');
 
-router.route('/products').get(isAuthenticatedUser, getProducts);
+router.route('/products').get(getProducts);
 router.route('/products/:id').get(getSingleProduct);
 
-router.route('/admin/products/new').post(newProduct);
-router.route('/admin/products/:id').put(updateProduct);
-
-router.route('/admin/products/:id').delete(deleteProduct);
+router.route('/admin/products/new').post(isAuthenticatedUser, newProduct);
+router.route('/admin/products/:id')
+			.put(isAuthenticatedUser, updateProduct)
+			.delete(isAuthenticatedUser, deleteProduct);
 
 
 module.exports = router;
