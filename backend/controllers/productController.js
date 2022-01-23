7a157442f35d55328f2c433ae302a21af0e735d8
sank48 <sanksrn48@gmail.com>
2022-01-23 17:49:30 +0530

    Adding user in Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,6 +5,9 @@ const APIFeature = require('../utils/apiFeatures')
 
 // Create new product => /api/v1/products/new
 exports.newProduct = catchAsyncError(async (req, res, next)=>{
+
+	req.body.user = req.user.id;
+
 	const product  = await Product.create(req.body);
 
 	res.status(201).json({
