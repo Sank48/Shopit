0700f849e867bedcbd576cef49e03f835308db1e
sank48 <sanksrn48@gmail.com>
2022-02-04 23:55:59 +0530

    Get single order by id or all orders of logged in user

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const {isAuthenticatedUser, authorizeRoles} = require('../middleware/auth')
-const {newOrder} = require('../controllers/orderController')
+const {newOrder,
+		getSingleOrder,
+		myOrder} = require('../controllers/orderController')
 
 router.route('/order/new').post(isAuthenticatedUser ,newOrder);
+
+router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
+router.route('/orders/me').get(isAuthenticatedUser, myOrder);
 
 module.exports = router;
