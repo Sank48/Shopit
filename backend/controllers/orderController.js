cb4a0e2e993bf5d7c5a6cc1322a599b8dbe5ea4d
sank48 <sanksrn48@gmail.com>
2022-02-10 14:29:51 +0530

    Update Process order - Admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -57,7 +57,7 @@ exports.myOrder = catchAsyncError(async(req, res, next)=>{
 	})
 })
 
-// Get all orders 'admin' => /api/vq/admin/orders/
+// Get all orders 'admin' => /api/v1/admin/orders/
 exports.allOrders = catchAsyncError(async(req, res, next)=>{
 	const order = await Order.find()
 
@@ -72,3 +72,36 @@ exports.allOrders = catchAsyncError(async(req, res, next)=>{
 		order
 	})
 })
+
+// Update order 'ADMIN' => /api/v1/admin/order/:id
+exports.updateOrder = catchAsyncError(async (req, res, next)=>{
+	const order = await Order.findById(req.params.id);
+
+	if(order.orderStatus === 'Delivered'){
+		return next(new ErrorHandler("You have already Delivered this order!", 400));
+	}
+
+	// updating product quantities
+	order.orderItems.forEach(async item =>{
+		await updateStock(item.product, item.quantity)
+	})
+
+	order.orderStatus = req.body.status,
+		order.deliveredAt = Date.now();
+
+	await order.save();
+
+	res.status(200).json({
+		success: true
+	})
+
+})
+
+
+async function updateStock(id, quantity){
+	const product = await Product.findById(id);
+
+	product.stock = product.stock - quantity;
+
+	await product.save({validateBeforeSave: false});
+}
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -5,12 +5,14 @@ const {isAuthenticatedUser, authorizeRoles} = require('../middleware/auth')
 const {newOrder,
 		getSingleOrder,
 		myOrder,
-		allOrders} = require('../controllers/orderController')
+		allOrders,
+		updateOrder} = require('../controllers/orderController')
 
 router.route('/order/new').post(isAuthenticatedUser ,newOrder);
 
 router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
 router.route('/orders/me').get(isAuthenticatedUser, myOrder);
 router.route('/admin/orders').get(isAuthenticatedUser, allOrders);
+router.route('/admin/order/:id').put(isAuthenticatedUser, updateOrder);
 
 module.exports = router;
